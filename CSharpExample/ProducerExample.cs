b1a87cf0ac2512031be86d751a5c5ad64f09a9f5
Vinny <vbonner@microsoft.com>
2023-10-13 13:59:52 -0700

    Update ProducerExample.cs

--- CSharpExample/ProducerExample.cs
+++ CSharpExample/ProducerExample.cs
@@ -176,7 +176,7 @@ namespace ProducerExample
 
         static void PrintUsage()
         {
-            Console.WriteLine("usage: .. produce <TopicName> <NumberOfMessageToSend> |createtopic <NameOfTopic>|deletetopic <TopicName>");
+            Console.WriteLine("usage: .. produce <TopicName> <NumberOfMessageToSend> |createtopic <NameOfTopic> <NumberOfPartitions>|deletetopic <TopicName>");
             System.Environment.Exit(1);
         }
 
